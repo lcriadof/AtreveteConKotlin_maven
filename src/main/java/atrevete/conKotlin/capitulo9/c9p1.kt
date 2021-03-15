8b4284729b47bf6931f50d7554918f4455dcf19e
Luis Criado <lcriadof@yahoo.es>
2021-03-15 17:53:51 +0100

    cap 9

---
+++ src/main/java/atrevete/conKotlin/capitulo9/c9p1.kt
@@ -0,0 +1,16 @@
+/*
+Atrévete con Kotlin (libro en preparación) ISBN: 9798596367164
+Autor: http://luis.criado.online/index.html
+ */
+package atrevete.conKotlin.capitulo9
+// rnago de numeros
+fun main() {
+    println("tipo,   tamaño,        rango ")
+    println("Byte,   "+Byte.SIZE_BITS+" bits, "+Byte.MIN_VALUE+" .. "+Byte.MAX_VALUE) // 1
+    println("Short, "+Short.SIZE_BITS+" bits, "+Short.MIN_VALUE+" .. "+Short.MAX_VALUE)
+    println("Int,   "+Int.SIZE_BITS+" bits, "+Int.MIN_VALUE+" .. "+Int.MAX_VALUE)
+    println("Long,  "+Long.SIZE_BITS+" bits, "+Long.MIN_VALUE+" .. "+Long.MAX_VALUE)
+    println("Float, "+Float.SIZE_BITS+" bits, "+Float.MIN_VALUE+" .. "+Float.MAX_VALUE)
+    println("Double,"+Double.SIZE_BITS+" bits, "+Double.MIN_VALUE+" .. "+Double.MAX_VALUE)
+
+}
