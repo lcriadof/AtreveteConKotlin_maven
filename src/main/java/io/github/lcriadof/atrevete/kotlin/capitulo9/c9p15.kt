0528c04ed69034b5ce8718c48019e0d476c91727
Luis Criado <lcriadof@yahoo.es>
2021-04-24 13:39:49 +0200

    actualizacion

--- src/main/java/io/github/lcriadof/atrevete/kotlin/capitulo9/c9p15.kt
+++ src/main/java/io/github/lcriadof/atrevete/kotlin/capitulo9/c9p15.kt
@@ -4,23 +4,22 @@ import com.github.psambit9791.jdsp.transform.DiscreteFourier
 import com.github.psambit9791.jdsp.transform.InverseDiscreteFourier
 import com.github.psambit9791.jdsp.io.Csv
 
+// https://psambit9791.github.io/jDSP/
+
 fun main(){
 
-    val signal= DoubleArray(9, { i -> i+1.0 })
+    val signal= DoubleArray(9, { i -> i+1.0 })  // 1
     println("Señal original")
     println(signal.joinToString())
 
-    val fft = DiscreteFourier(signal)
-    fft.dft()
-
-    val onlyPositive = false
-    val out = fft.returnFull(onlyPositive) //Full Complex
-
+    val fft = DiscreteFourier(signal) // 2
+    fft.dft() // 3
+    val out = fft.returnFull(false) // 3 Full Complex
 
     println()
     println("procesamos la señal original con la FFT")
     // El resultado FFT tiene números complejos
-    for (i in 0..out.size-1){
+    for (i in 0..out.size-1){ // 4
         for (j in 0..1){
             print("\t"+out[i][j])
             if (j==1){
@@ -33,20 +32,20 @@ fun main(){
 
     println()
     println("Recuperamos la señal original con la transformada inversa IFFT")
-    val transformer = InverseDiscreteFourier(out,false)
-    transformer.idft()
-    val out2 = transformer.absoluteSignal
-    println(out2.joinToString())
+    val transformer = InverseDiscreteFourier(out,false) // 5
+    transformer.idft() // 6
+    val out2 = transformer.absoluteSignal // 7
+    println(out2.joinToString()) // 8
 
 
 
     // redondeamos los números
-    for (i in 0..out2.size-1){
+    for (i in 0..out2.size-1){ // 9
         out2[i] = (Math.round(out2[i] * 100) / 100).toDouble()
     }
     println()
     println("Recuperamos la señal original con la transformada inversa IFFT (redondeado)")
-    println(out2.joinToString())
+    println(out2.joinToString()) // 10
 
 
 }
